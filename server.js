b31ec2dbb29184029538c60e863753645357340e
Kendra Staver <staverkendra@gmail.com>
2021-10-17 23:30:48 -0700

    attempting to fix an error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 3001; // Make 3001 the port to the api server
 const fs = require('fs'); // For write to file
 const path = require('path');
+const _dirname = path.resolve();
 
 // Express.js base code
 const express = require('express'); // Added because express is an npm package
@@ -48,7 +49,7 @@ function createNewNote(body, notesArray){
 }
 
 app.post('/api/notes', (req,res) =>{
-    req.body.id = note.length.toString();
+   // req.body.id = note.length.toString();
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
 });
