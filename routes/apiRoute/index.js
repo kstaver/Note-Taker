5e02755b085b0c1256db9fd95e339f8e3c75d852
Kendra Staver <staverkendra@gmail.com>
2021-10-18 00:45:48 -0700

    fixed varaible issue

--- routes/apiRoute/index.js
+++ routes/apiRoute/index.js
@@ -37,8 +37,8 @@ function deleteNote(){
     for(let i = 0; i < note.length; i++){
         let notes = note[i];
 
-        if(notes.id == id){
-            notesArray.splice(i, 1);
+        if(notes.id === id){
+            note.splice(i, 1);
             fs.writeFileSync(
                 path.join(__dirname, '../../db/db.json'),
                 JSON.stringify(note, null, 2)
@@ -48,7 +48,7 @@ function deleteNote(){
     }
 };
 
-router.get('/notes', (req,res) =>{
+/*router.get('/notes', (req,res) =>{
     const delNote = deleteNote(req.body, note);
     res.json(delNote);
 });
@@ -57,7 +57,7 @@ router.post('/notes', (req, res) =>{
     req.body.id = note.length.toString();
     const delNote = createNewNote(req.body, note);
     res.json(delNote);
-});
+});*/
 
 router.delete('/notes/:id', (req, res) => {
     deleteNote(req.params.id, note);
