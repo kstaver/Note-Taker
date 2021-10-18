7031d8e0706cc31a1556e12c5523648b3b1af9fb
Kendra Staver <staverkendra@gmail.com>
2021-10-17 22:40:50 -0700

    finished create newnote and deltenote

--- server.js
+++ server.js
@@ -28,13 +28,49 @@ app.get('*', (req,res) => {
     res.sendFile(path.join(_dirname, './public/index.html'));
 });
 
-function newNote(){
+function createNewNote(body, notesArray){
+    const newNote = body;
+    if (!Array.isArray(notesArray)){
+        notesArray = [];
+    }
+    if(notesArray.length === 0){
+        notesArray.push(0);
+    }
+    body.id = notesArray[0];
+    notesArray[0]++;
 
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(_dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
 }
+
+app.post('/api/notes',(req,res) =>{
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
 
 function deleteNote(){
-    
+    for(let i = 0; i < notesArray.length; i++){
+        let node = notesArray[i];
+
+        if(note.id == id){
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(_dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
 }
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.join(true);
+});
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
