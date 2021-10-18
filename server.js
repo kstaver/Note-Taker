5fe93c68bab63d597bddad1ad887ada40bc85bf1
Kendra Staver <staverkendra@gmail.com>
2021-10-17 23:45:21 -0700

    parsing server.js into two routers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3001; // Make 3001 the port to the api server
 const fs = require('fs'); // For write to file
 const path = require('path');
-const _dirname = path.resolve();
+//const _dirname = path.resolve();
 
 // Express.js base code
 const express = require('express'); // Added because express is an npm package
