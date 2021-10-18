2dad07d1991451c2fe17ec53fd131626c23505bb
Kendra Staver <staverkendra@gmail.com>
2021-10-17 22:18:35 -0700

    created sendfiles to join the notes and index

--- server.js
+++ server.js
@@ -6,7 +6,31 @@ const path = require('path');
 const express = require('express'); // Added because express is an npm package
 const app = express();
 
+const allNotes = require("./db/db.json");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) =>{
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(_dirname, './public/index.html'));
+});
+
+app.get('/notes', (req,res) =>{
+    res.sendFile(path.join(_dirname, './public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(_dirname, './public/index.html'));
+});
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
-  });
+});
+
+
 
