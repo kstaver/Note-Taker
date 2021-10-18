25bf8400e488894e5de8ecdc51157ad50db709e3
Kendra Staver <staverkendra@gmail.com>
2021-10-18 00:52:29 -0700

    testing out different deletes

--- routes/apiRoute/index.js
+++ routes/apiRoute/index.js
@@ -34,34 +34,28 @@ router.post('/notes', (req, res) =>{
 });
 
 function deleteNote(){
-    for(let i = 0; i < note.length; i++){
-        let notes = note[i];
-
-        if(notes.id === id){
-            note.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, '../../db/db.json'),
-                JSON.stringify(note, null, 2)
-            );
-            break;
-        }
-    }
+  //get the note to delete
+  const note = notes.filter((n) => n.id === noteId)[0];
+  if (note) {
+    //get the note  index from the notes
+    const noteIndex = notes.indexOf(note);
+    //delete the note from the notes array
+    notes.splice(noteIndex, 1);
+    //write array in the file
+    writeFile(notes);
+    return note;
+  }
 };
 
-/*router.get('/notes', (req,res) =>{
-    const delNote = deleteNote(req.body, note);
-    res.json(delNote);
-});
-
-router.post('/notes', (req, res) =>{
-    req.body.id = note.length.toString();
-    const delNote = createNewNote(req.body, note);
-    res.json(delNote);
-});*/
 
 router.delete('/notes/:id', (req, res) => {
-    deleteNote(req.params.id, note);
-    res.json(true);
+    const { id } = req.params;
+    const note = deleteNote(id, notes);
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(400).send("Didn't find any notes with that ID.");
+    }
 });
 
 module.exports = router;
