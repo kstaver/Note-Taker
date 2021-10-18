65d717c173fc96662b7df8869f4b53c8528c2a06
Kendra Staver <staverkendra@gmail.com>
2021-10-18 00:17:43 -0700

    trying to see if validation is broken

--- routes/apiRoute/index.js
+++ routes/apiRoute/index.js
@@ -5,14 +5,14 @@ const notesArray = require('../../db/db.json');
 
 function createNewNote(body, notesArray){
     const newNote = body;
-    if (!Array.isArray(notesArray)){
+    /*if (!Array.isArray(notesArray)){
         notesArray = [];
     }
     if(notesArray.length === 0){
         notesArray.push(0);
     }
     body.id = notesArray[0];
-    notesArray[0]++;
+    notesArray[0]++;*/
 
     notesArray.push(newNote);
     fs.writeFileSync(
