2c27a42efd409f29537dfa4caa226de9713228e5
Kendra Staver <staverkendra@gmail.com>
2021-10-17 23:59:30 -0700

    simplified eveyrthing by making routers

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 const PORT = process.env.PORT || 3001; // Make 3001 the port to the api server
 const fs = require('fs'); // For write to file
 const path = require('path');
+const apiRoute = require('./routes/apiRoutes');
+const htmlRoute = require('./routes/htmlRoutes');
 
 // Express.js base code
 const express = require('express'); // Added because express is an npm package
 const app = express();
 
-const allNotes = require("./db/db.json");
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) =>{
-    res.json(allNotes.slice(1));
-});
+app.use(express.static('public'));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(_dirname, './public/index.html'));
-});
-
-app.get('/notes', (req,res) =>{
-    res.sendFile(path.join(_dirname, './public/notes.html'));
-});
-
-app.get('*', (req,res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
-});
+const {newNote} = require('./db/db.json');
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
