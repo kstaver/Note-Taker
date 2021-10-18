8295dd4d6f7654fad1e212798d6f106e0b1bbaa6
Kendra Staver <staverkendra@gmail.com>
2021-10-18 00:58:54 -0700

    testing to see if comments broke app

--- routes/apiRoute/index.js
+++ routes/apiRoute/index.js
@@ -5,14 +5,6 @@ const note = require('../../db/db.json');
 
 function createNewNote(body, note){
     const notes = body;
-    /*if (!Array.isArray(notesArray)){
-        notesArray = [];
-    }
-    if(notesArray.length === 0){
-        notesArray.push(0);
-    }
-    body.id = notesArray[0];
-    notesArray[0]++;*/
 
     note.push(notes);
     fs.writeFileSync(
@@ -23,7 +15,6 @@ function createNewNote(body, note){
 };
 
 router.get('/notes', (req,res) =>{
-    //const newNote = createNewNote(req.body, allNotes);
     res.json(note);
 });
 
@@ -33,29 +24,29 @@ router.post('/notes', (req, res) =>{
     res.json(notes);
 });
 
-// function deleteNote(){
-//   //get the note to delete
-//   const note = notes.filter((n) => n.id === noteId)[0];
-//   if (note) {
-//     //get the note  index from the notes
-//     const noteIndex = notes.indexOf(note);
-//     //delete the note from the notes array
-//     notes.splice(noteIndex, 1);
-//     //write array in the file
-//     writeFile(notes);
-//     return note;
-//   }
-// };
+function deleteNote(){
+  //get the note to delete
+  const note = notes.filter((n) => n.id === noteId)[0];
+  if (note) {
+    //get the note  index from the notes
+    const noteIndex = notes.indexOf(note);
+    //delete the note from the notes array
+    notes.splice(noteIndex, 1);
+    //write array in the file
+    writeFile(notes);
+    return note;
+  }
+};
 
 
-// router.delete('/notes/:id', (req, res) => {
-//     const { id } = req.params;
-//     const note = deleteNote(id, notes);
-//     if (note) {
-//       res.json(note);
-//     } else {
-//       res.status(400).send("Didn't find any notes with that ID.");
-//     }
-// });
+router.delete('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    const note = deleteNote(id, notes);
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(400).send("Didn't find any notes with that ID.");
+    }
+});
 
 module.exports = router;
