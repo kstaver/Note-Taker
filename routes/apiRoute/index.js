bae63296bddbaa9ca4593a64041f61efaa03fbe7
Kendra Staver <staverkendra@gmail.com>
2021-10-18 01:18:47 -0700

    cannot figure out how to delete

--- routes/apiRoute/index.js
+++ routes/apiRoute/index.js
@@ -11,7 +11,6 @@ function createNewNote(body, note){
         path.join(__dirname, '../../db/db.json'),
         JSON.stringify(note, null, 2)
     );
-//    return newNote;
 };
 
 router.get('/notes', (req,res) =>{
@@ -24,29 +23,26 @@ router.post('/notes', (req, res) =>{
     res.json(notes);
 });
 
-function deleteNote(){
-  //get the note to delete
-  const note = notes.filter((n) => n.id === noteId)[0];
-  if (note) {
-    //get the note  index from the notes
-    const noteIndex = notes.indexOf(note);
-    //delete the note from the notes array
-    notes.splice(noteIndex, 1);
-    //write array in the file
-    writeFile(notes);
-    return note;
-  }
-};
+// function deleteNote(){
+// 
+//   const note = notes.filter((n) => n.id === noteId)[0];
+//   if (note) {
+//     const noteIndex = notes.indexOf(note);
+//     notes.splice(noteIndex, 1);
+//     writeFile(notes);
+//     return note;
+//   }
+// };
 
 
-router.delete('/notes/:id', (req, res) => {
-    const { id } = req.params;
-    const note = deleteNote(id, notes);
-    if (note) {
-      res.json(note);
-    } else {
-      res.status(400).send("Didn't find any notes with that ID.");
-    }
-});
+// router.delete('/notes/:id', (req, res) => {
+//     const { id } = req.params;
+//     const note = deleteNote(id, notes);
+//     if (note) {
+//       res.json(note);
+//     } else {
+//       res.status(400).send("Didn't find any notes with that ID.");
+//     }
+// });
 
 module.exports = router;
