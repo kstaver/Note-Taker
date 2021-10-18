f9a6b2fcf1ad4b561929f32abbf65f491cf16cd4
Kendra Staver <staverkendra@gmail.com>
2021-10-18 00:11:57 -0700

    fixed a bunch of spelling issues

---
+++ routes/apiRoute/index.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const notesArray = require('../../db/db.json');
+
+function createNewNote(body, notesArray){
+    const newNote = body;
+    if (!Array.isArray(notesArray)){
+        notesArray = [];
+    }
+    if(notesArray.length === 0){
+        notesArray.push(0);
+    }S
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(_dirname, '../../db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+//    return newNote;
+};
+
+router.get('/notes', (req,res) =>{
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+router.post('/notes', (req, res) =>{
+    req.body.id = notesArray.length.toString();
+    const newNote = createNewNote(req.body, notesArray);
+    res.json(newNote);
+});
+
+
+/*
+function deleteNote(){
+    for(let i = 0; i < notesArray.length; i++){
+        let note = notesArray[i];
+
+        if(note.id == id){
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(_dirname, '../../db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
+};
+
+router.get('/notes', (req,res) =>{
+    const delNote = deleteNote(req.body, allNotes);
+    res.json(delNote);
+});
+
+router.post('/notes', (req, res) =>{
+    req.body.id = note.length.toString();
+    const delNote = createNewNote(req.body, note);
+    res.json(delNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+*/
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const PORT = process.env.PORT || 3001; // Make 3001 the port to the api server
 const fs = require('fs'); // For write to file
 const path = require('path');
-const apiRoute = require('./routes/apiRoutes');
-const htmlRoute = require('./routes/htmlRoutes');
+const apiRoute = require('./routes/apiRoute');
+const htmlRoute = require('./routes/htmlRoute');
 
 // Express.js base code
 const express = require('express'); // Added because express is an npm package
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.use('/api', apiRoute);
 app.use('/', htmlRoute);
 
-const {newNote} = require('./db/db.json');
+const {notes} = require('./db/db.json');
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
